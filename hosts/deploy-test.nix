2e93d795bcd175fc0ec20bad63667db6dfb23715
jconnuck <johnconnuck@gmail.com>
2025-07-10 01:11:15 -0400

    Add SSH keys for deployment access
    - Add jconnuck@mac-bk SSH key to nixos and root users
    - Change password to initialPassword for proper setup
    - This will allow SSH access after deployment

--- hosts/deploy-test.nix
+++ hosts/deploy-test.nix
@@ -43,9 +43,17 @@
   # Create nixos user with password for deployment
   users.users.nixos = {
     isNormalUser = true;
-    password = "test123";
+    initialPassword = "test123";  # Using initialPassword instead of password
     extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCjZ2yKqEb+s4gz8It2vSNNnnZIJKs0GZsCdCJIUByk4Np5kqI7oi7NIPbzjOa5PLOhucGL/JyIi84Tr/0jr0to/1Ifc/iVXevjdhDsTvxxZkLCNl/GwGWflh59oFAyZ1whceKWYLOiU4su4q+OjdsaZDjHbtZVAppcoQf+u1hjvN1jmhrxaiGD8koUBjbsk2E4EnV2JjgqGoZYp3ujXf2q0xp/6yUrTyOJZlclee0Zd/Jf/mgiBOgWCXs7hQuAm8cO7fq00rQL+RINebqPIHGJUxXDnqsI6Qd+zn2x4vNy9D2BFZlmcR8S9K+2nHcYGSa4ROxQ4BLLgGZR3/Q019FeLsvXAoR2wwoFLLF/TEu1VMJlTN8ASSrMia5BdPdMMOh+uzZ3DyVvmKIN54NDXIdjyVQoF/FijwtRiTNBIj1MT87c7AmNNIGlBmBfduhbo9bnj/StFcYWODAR9KIkh1jr1RJhZ3fIdqY/7JTV5658uztBiZ+l2Tb4A2qCww9Kb2M= jconnuck@mac-bk"
+    ];
   };
+  
+  # Also add SSH key to root for easier access
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCjZ2yKqEb+s4gz8It2vSNNnnZIJKs0GZsCdCJIUByk4Np5kqI7oi7NIPbzjOa5PLOhucGL/JyIi84Tr/0jr0to/1Ifc/iVXevjdhDsTvxxZkLCNl/GwGWflh59oFAyZ1whceKWYLOiU4su4q+OjdsaZDjHbtZVAppcoQf+u1hjvN1jmhrxaiGD8koUBjbsk2E4EnV2JjgqGoZYp3ujXf2q0xp/6yUrTyOJZlclee0Zd/Jf/mgiBOgWCXs7hQuAm8cO7fq00rQL+RINebqPIHGJUxXDnqsI6Qd+zn2x4vNy9D2BFZlmcR8S9K+2nHcYGSa4ROxQ4BLLgGZR3/Q019FeLsvXAoR2wwoFLLF/TEu1VMJlTN8ASSrMia5BdPdMMOh+uzZ3DyVvmKIN54NDXIdjyVQoF/FijwtRiTNBIj1MT87c7AmNNIGlBmBfduhbo9bnj/StFcYWODAR9KIkh1jr1RJhZ3fIdqY/7JTV5658uztBiZ+l2Tb4A2qCww9Kb2M= jconnuck@mac-bk"
+  ];
 
   # Enable sudo without password for nixos user (needed for deployment)
   security.sudo.wheelNeedsPassword = false;
