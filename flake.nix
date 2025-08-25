ddccb6137dafbd6b2cce64a842bc5e71bb5d022e
jconnuck <johnconnuck@gmail.com>
2025-08-25 00:52:41 -0400

    Add nixos-generators for VM testing with DRY configuration
    - Add nixos-generators input to flake for creating bootable ISOs
    - Use idiomatic let...in pattern to eliminate duplication
    - Extract common system and modules variables
    - Create cistern-iso package for both x86_64-linux and aarch64-darwin
    - Update flake.lock with nixos-generators dependencies
    
    Enables VM testing workflow: nix build .#cistern-iso

--- flake.nix
+++ flake.nix
@@ -6,16 +6,40 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     # Nixarr media server stack
     nixarr.url = "github:rasmus-kirk/nixarr";
+    # NixOS generators for creating VM images
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   
-  outputs = { self, nixpkgs, nixarr }: {
+  outputs = { self, nixpkgs, nixarr, nixos-generators }: 
+  let
+    # Target architecture for Cistern deployments
+    system = "x86_64-linux";
+    
+    # Core Cistern media server modules
+    modules = [
+      ./configuration.nix
+      ./modules/base.nix
+      nixarr.nixosModules.default
+    ];
+  in
+  {
     nixosConfigurations.cistern = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ 
-        ./configuration.nix
-        ./modules/base.nix
-        nixarr.nixosModules.default
-      ];
+      inherit system modules;
+    };
+
+    # VM images for testing
+    packages.x86_64-linux.cistern-iso = nixos-generators.nixosGenerate {
+      inherit system modules;
+      format = "iso";
+    };
+
+    # Also provide packages for macOS (for cross-building)
+    packages.aarch64-darwin.cistern-iso = nixos-generators.nixosGenerate {
+      inherit system modules;
+      format = "iso";
     };
   };
 }
